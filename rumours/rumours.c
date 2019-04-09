9493ef8a467b54b7bf64b164382e9b0451591e14
js395 <js395@st-andrews.ac.uk>
2019-04-09 16:40:05 +0100

    fork testing

--- rumours/rumours.c
+++ rumours/rumours.c
@@ -1,13 +1,21 @@
 /**
- * @file rumours.c
- * @author your student ID
- * @brief 
- * 
- */
+* @file rumours.c
+* @author 180012798
+* @brief
+*
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h> 
 
-
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
-
-   return 0;
+  int id = fork();
+  if (id == 0) {
+    printf("%s\n", argv[1]);
+  } else {
+    printf("%s\n", argv[2]);
+  }
+  return 0;
 }
