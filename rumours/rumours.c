5694aee6327bf68f4417ae9030f6fcc3fcb36b6d
js395 <js395@st-andrews.ac.uk>
2019-04-11 19:38:06 +0100

    Commenting added

--- rumours/rumours.c
+++ rumours/rumours.c
@@ -13,27 +13,23 @@ int main(int argc, char *argv[])
   int fd[2];
   char* str;
   // printf("%s\n", string );
-  for(int i = 1; i <= argc - 1; i++) {
+  for(int i = 1; i <= argc - 1; i++) { //combines the command line arguments into a single string
     str = realloc(str, strlen(argv[i]));
     strcat(str, argv[i]);
     strcat(str, " ");
   }
-  int length = strlen(str);
-  srand(time(NULL));
+  int length = strlen(str); //calculates the length of the string so strlen isn't called multiple times
+  srand(time(NULL)); // seeds the rand() function
   pipe(fd);
-  while (count < 10 && child_id != 0) {
+  while (count < 10 && child_id != 0) { //controls the number of children made and ensures when a child becomes a parent the process closes.
     if (count != 0) {
-
       read(fd[1], str, sizeof(str));
       parent_id = child_id;
       printf("pid: %d  received string: %s\n", getpid(), str );
     } else {
       parent_id = getpid();
     }
-    // printf("in loop %d\n", count);
-    // printf("PID:  %d\n",child_id);
     child_id = fork();
-
     if (child_id != 0) { //parent
       printf("pid: %d", parent_id);
       stringswap(str, length);
@@ -45,6 +41,13 @@ int main(int argc, char *argv[])
   }
   return 0;
 }
+
+/**
+This function takes in a string and swaps two random characters.
+
+@Param string the string which has two character swapped.
+@Param string_length the length of the string argument.
+*/
 
 void stringswap(char* string, int string_length){
   int index1, index2;
