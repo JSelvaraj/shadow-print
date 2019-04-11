88fdc2f55f68fe763aa6f1e3172ce07090c0c269
js395 <js395@st-andrews.ac.uk>
2019-04-11 18:04:18 +0100

    corrected header files

--- rumours/rumours.c
+++ rumours/rumours.c
@@ -4,14 +4,6 @@
 * @brief
 *
 */
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <fcntl.h>
 #include "rumours.h"
 
 int count = 0;
