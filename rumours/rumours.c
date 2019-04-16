636adb3949019d87c127e8f8aa34682e299cd55e
js395 <js395@st-andrews.ac.uk>
2019-04-16 20:14:32 +0100

    removed extranous commenting

--- rumours/rumours.c
+++ rumours/rumours.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
   int child_id = 0, parent_id = getpid();
   int fd[2];
   char* str;
-  // printf("%s\n", string );
+
   for(int i = 1; i <= argc - 1; i++) { //combines the command line arguments into a single string
     str = realloc(str, strlen(argv[i]));
     strcat(str, argv[i]);
@@ -26,34 +26,26 @@ int main(int argc, char *argv[])
   pipe(fd);
   while (!child_id) { //controls the number of children made and ensures when a child becomes a parent the process closes. !child_id same as child_id == 0
     if (count != 0) {
-      // close(fd[1]);
       read(fd[0], str, length);
-
       strcpy(string, str);
       parent_id = getpid();
-      printf("pid: %d  received string: %s loop number: %d\n", getpid(), string, count);
+      printf("pid: %d  received string: %s child process number: %d\n", getpid(), string, count);
     }
-    
+
     count++;
     if (count < NUM_OF_PROCESSES + 1) child_id = fork();
     else break;
 
     if (child_id != 0) { //parent
-      // close(fd[0]);
       srand(time(NULL) + count); // seeds the rand() function
       str = stringswap(string, length);
-      // printf("modified string 2: %s\n", str );
       write(fd[1], str, length);
     } else { //child
       printf("New Process: %d   Parent: %d\n", getpid() , parent_id);
     }
-
   }
-  if(count != NUM_OF_PROCESSES + 1) { // formatting so the terminal directory doesn't intersect stdout
-  wait(child_id);
-}
-
-return 0;
+  if(count != NUM_OF_PROCESSES + 1) wait(NULL); // formatting so the terminal directory doesn't intersect stdout
+  return 0;
 }
 
 /**
@@ -73,10 +65,6 @@ char* stringswap(char string[], int string_length){
   char temp;
   temp = string[index1];
   string[index1] = string[index2];
-  // printf("index: %c\n",string[index2]);
-  // printf("temp: %c\n", temp);
   string[index2] = temp;
-  // printf("index2: %c\n",string[index2]);
-  // printf("modified string: %s\n", string );
   return string;
 }
